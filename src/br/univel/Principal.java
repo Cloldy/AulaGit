5651c3147948a7f933e14a8df48bfbb207502329
Luiz Gustavo de Araujo Paiva <luizgustavopaiva97@hotmail.com>
2016-03-15 19:58:08 -0300

    Liberação versão

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -2,6 +2,11 @@ package br.univel;
 
 public class Principal {
 
+/**
+ * Versão 1.0
+ * 
+ * @author LGDAPAIVA	
+ */
 	
 	public static void main(String[] args) {
 		System.out.println("Fala ae mano das quebradas");
